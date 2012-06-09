536bf09744da47d4a5bb9f6cb53864b90b45c0d1
Tyler Tringas <ttringas@gmail.com>
2012-06-09 16:04:54 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sunride::Application.config.secret_token = '1a74992c3796f98dcc877d14b9e57b38f3862d35c012189c89362420bbf109d9962e4839e00ca038dc2121b9dd6e3145bb4da5ef9a3394d000740577b7abf80d'
